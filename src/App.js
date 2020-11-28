c855b9af919b4ad7b1bae05af73587de49ae77d4
Leah H <harrichl@gmail.com>
2020-11-28 05:29:27 -0500

    move menu into components and add styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import HomePage from "./homepage.component";
-
 import "./App.css";
+
+import HomePage from "./pages/homepage/homepage.component";
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <HomePage />
     </div>
   );
---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import MenuItem from "../menu-item/menu-item.component";
+
+import "./directory.styles.scss";
+
+class DirectoryMenu extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      sections: [
+        {
+          title: "coffee",
+          imageUrl:
+            "https://images.unsplash.com/photo-1422207109431-97544339f995?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=2700&q=80",
+          id: 1,
+          linkUrl: "shop/coffee",
+        },
+        {
+          title: "brewing",
+          imageUrl:
+            "https://images.unsplash.com/photo-1528800190552-c3aae2994f70?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=2700&q=80",
+          id: 2,
+          linkUrl: "shop/brewing",
+        },
+        {
+          title: "giftcards",
+          imageUrl:
+            "https://images.unsplash.com/photo-1572798793834-67d5e285760d?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=2767&q=80",
+          id: 3,
+          linkUrl: "shop/giftcards",
+        },
+      ],
+    };
+  }
+
+  render() {
+    return (
+      <div className="directory-menu">
+        {this.state.sections.map(({ title, imageUrl, id }) => (
+          <MenuItem key={id} title={title} imageUrl={imageUrl} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default DirectoryMenu;
